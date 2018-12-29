dcf477d47ea33fa168874989d50b699edfd993af
cvaquedano <chvb2002@hotmail.com>
2018-12-29 06:27:17 -0600

    Add base Entity Person

---
+++ AcademicManagement.Domain/Entities/BaseEntities/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AcademicManagement.Domain.Entities.BaseEntities
+{
+    public class Person
+    {
+       
+        public string Name { get; set; }
+       
+        public string LastName { get; set; }
+      
+        public DateTime BirthDate { get; set; }
+
+        public bool Gender { get; set; }
+
+        public string NombreCompleto()
+        {
+            return Name + " " + LastName;
+        }
+    }
+}
